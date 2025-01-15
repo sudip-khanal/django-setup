d51551aa22481c21addc7cdd27c8bb2e165b6a8f
sudip-khanal <sudipkhanal0777@gmail.com>
2025-01-15 11:43:02 +0545

    project setup

---
+++ apps/book/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.full_name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    categories = models.ManyToManyField(Category, related_name="books")
+    description = models.TextField()
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
